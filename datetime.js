241dcfc110804b6a5c466deee323798e1b3f56f9
Trobo <57040351+trobonox@users.noreply.github.com>
2021-12-07 16:43:51 +0100

    remove file with different name

--- datetime.js
+++
@@ -1,9 +0,0 @@
-function updateClock(){
-    var dt = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-    document.getElementById("time").innerHTML = dt.toLocaleTimeString('de-DE');
-    document.getElementById("date").innerHTML = dt.toLocaleDateString('de-DE', options); 
-}
-
-setInterval(updateClock, 0);
